c55daab533fe5a5aac5879e17c20e4184acfa6c6
danylopriadko9 <91220166+danylopriadko@users.noreply.github.com>
2022-10-15 16:02:10 +0200

    delete server side

--- server/controllers/index.js
+++
@@ -1,4 +0,0 @@
-export * as productController from './productController.js';
-export * as categoriesController from './categoriesController.js';
-export * as newsController from './newsController.js';
-export * as historyController from './historyController.js';
--- server/response.js
+++
@@ -1,9 +0,0 @@
-exports.status = (values, res) => {
-  const data = {
-    status: 200,
-    values,
-  };
-
-  data.json(data);
-  data.end();
-};
